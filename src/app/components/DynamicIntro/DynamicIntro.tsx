0bc34c68ddf58d83d711a242c3f7dbaa607522c0
Jocelyn Wensloff <jewensloff@gmail.com>
2024-01-14 20:57:18 -0700

    feat: Add styling to intro page

--- src/app/components/DynamicIntro/DynamicIntro.tsx
+++ src/app/components/DynamicIntro/DynamicIntro.tsx
@@ -1,21 +1,24 @@
+import React from 'react';
 import Image from 'next/image';
 import styles from './DynamicIntro.module.css';
 
 function DynamicIntro() {
   return (
     <section className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src='/jw-high-resolution-logo.jpg'
-          alt='JW logo'
-          height={175}
-          width={175}
-          className={styles.img}
-        />
-      </div>
-      <div className={styles.textWrapper}>
-        <h1>Jocelyn Wensloff</h1>
-        <p>Software Engineer | Biologist | Genuine Human</p>
+      <div className={styles.innerContainer}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/jw-high-resolution-logo.jpg'
+            alt='JW logo'
+            height={175}
+            width={175}
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.textWrapper}>
+          <h1>Jocelyn Wensloff</h1>
+          <p>Software Engineer | Biologist | Genuine Human</p>
+        </div>
       </div>
     </section>
   );
