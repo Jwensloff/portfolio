bf137c62a70f541d5ff8346eeee0cb44ee442dd1
Jocelyn Wensloff <jewensloff@gmail.com>
2024-01-16 15:09:39 -0700

    feat: Add resume to about me page

--- src/app/AboutMe/AboutMe.tsx
+++ src/app/AboutMe/AboutMe.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './AboutMe.module.css';
+import Link from 'next/link';
 
 function AboutMe() {
   return (
     <section className={styles.container}>
       <div className={styles.innerContainer}>
-        <div className={styles.header}>
-          <h1>A bit about me...</h1>
-        </div>
-        <div className={styles.profileContainer}>
+        {/* <div className={styles.profileContainer}> */}
           <div className={styles.imgContainer}>
+            {/* <div className={styles.header}> */}
+              <h1>A bit about me...</h1>
+            {/* </div> */}
             <Image
               src='/ProfileImg.jpg'
               alt='Jocelyn and her dog Dusty'
@@ -18,32 +19,40 @@ function AboutMe() {
               width={325}
               className={styles.image}
             />
-            <p>View Resume</p>
+            <div className={styles.resumeButtonContainer}>
+              <a
+                className={styles.resumeButton}
+                href='/resume/Wensloff_Resume.pdf'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                View Resume
+              </a>
+            </div>
           </div>
           <div className={styles.textContainer}>
-            <p>Software Engineer | Biologist | </p>
+            <h2>Software Engineer | Biologist | Problem Solver</h2>
             <p>
-              As a software engineer with roots in biology and ecology, I bring
-              a uique perspective to coding. Drawing parallels between the
-              biological and coding realms, I've honed a unique perspective that
-              enables me to perceive how a line of code can exert influence on
-              the broader application, akin to the way an organism shapes its
+              As a <span> software engineer </span>with roots in biology and
+              ecology, I bring a unique perspective to coding. Drawing parallels
+              between the biological and coding realms, I've honed a perspective
+              that enables me to perceive how a line of code can influence the
+              broader application, akin to the way an organism can impact its
               environment.
             </p>
 
-            <p className={styles.secondP}>
-              This lens has been instrumental in my ability to navigate the
-              complexities of software engineering. It has empowered me to not
-              only identify potential issues at the code level but also
-              anticipate their broader implications on the overall application.
-              By leveraging this holistic approach, I have achieved a heightened
-              awareness of the interconnected nature of software components,
-              allowing me to craft solutions that consider the entire ecosystem
-              of an application.
+            <p>
+              My background has allowed me to quickly learn and adapt to new
+              technologies, communicate effectively within a team, and apply an
+              <span> agile approach </span>to the development process. Combining
+              my cumulative skill set, I am eager to apply my abilities to forge
+              innovative solutions in climate, education, financial, or
+              customer-oriented technology. If you would like to chat more,
+              please feel free to contact me!
             </p>
           </div>
-        </div>
-            {/* <div className={styles.logoContainer}>
+        {/* </div> */}
+        {/* <div className={styles.logoContainer}>
               <Image src='/react-blue.png' alt='' height={50} width={50} />
               <Image src='/nextjs.png' alt='' height={50} width={60} />
               <Image src='/TypeScript.png' alt='' height={50} width={50} />
