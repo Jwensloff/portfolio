6192178182bc1ee004fa7c1b20750db5d472543e
Jocelyn Wensloff <jewensloff@gmail.com>
2024-01-22 18:16:28 -0700

    feat: Remove content

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -12,53 +12,8 @@ type ProjectsProps = {
 };
 
 const Projects = ({ projectsRef }: ProjectsProps) => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // width: 75,
-    flex: 1,
-    nextArrow: (
-      <Arrow
-        className={''}
-        style={undefined}
-        onClick={function (
-          event: React.MouseEvent<HTMLDivElement, MouseEvent>
-        ): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    ),
-    prevArrow: (
-      <Arrow
-        className={''}
-        style={undefined}
-        onClick={function (
-          event: React.MouseEvent<HTMLDivElement, MouseEvent>
-        ): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    ),
-    initialSlide: 0,
-  };
+  
 
-  function Arrow(props: {
-    className: string;
-    style: React.CSSProperties | undefined;
-    onClick: React.MouseEventHandler<HTMLDivElement>;
-  }) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: 'block' }}
-        onClick={onClick}
-      />
-    );
-  }
   const projects = projectData.map((proj) => {
     return (
       <div className={styles.individual} key={proj.name}>
@@ -83,22 +38,7 @@ const Projects = ({ projectsRef }: ProjectsProps) => {
   return (
     <section className={styles.mainContainer} ref={projectsRef as React.RefObject<HTMLElement>}>
       <div className={styles.innerContainer}>
-        <div className={styles.topContent}>
-          <div className={styles.textContainer}>
-            <p>
-              Here are a few projects that I have had the pleasure of creating
-              either collaboratively or as a solo project. Each project
-              represents a unique journey of creativity and problem-solving,
-              reflecting my passion for building meaningful solutions with a
-              clean, deliberate aesthetic.
-            </p>
-          </div>
-          <div className={styles.sliderContainer}>
-            <div className='container'>
-              <Slider {...settings}>{projects}</Slider>
-            </div>
-          </div>
-        </div>
+        
       </div>
     </section>
   );
