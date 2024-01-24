70ba440b125a909654cb11faf581a7183ba2b59c
Jocelyn Wensloff <jewensloff@gmail.com>
2024-01-24 13:45:41 -0700

    feat: Change styling in about me

--- src/app/AboutMe/AboutMe.tsx
+++ src/app/AboutMe/AboutMe.tsx
@@ -13,9 +13,8 @@ const AboutMe = ({ aboutRef }: AboutMeProps) => {
       className={styles.container}
     >
       <div className={styles.innerContainer}>
-          <h2>Software Engineer | Biologist | Problem Solver</h2>
+        <h1>Hi, I'm Jocelyn.</h1>
         <div className={styles.content}>
-
           <div className={styles.leftContent}>
             <div className={styles.imgContainer}>
               <Image
@@ -25,7 +24,6 @@ const AboutMe = ({ aboutRef }: AboutMeProps) => {
                 className={styles.image}
               />
             </div>
-            {/* <div className={styles.resumeButtonContainer}> */}
             <a
               className={styles.resumeButton}
               href='/resume/Wensloff_Resume.pdf'
@@ -34,39 +32,27 @@ const AboutMe = ({ aboutRef }: AboutMeProps) => {
             >
               View Resume
             </a>
-            {/* </div> */}
           </div>
           <div className={styles.textContainer}>
-            {/* <h1>Jocelyn</h1> */}
-            {/* <h2>Software Engineer | Biologist | Problem Solver</h2> */}
-            <p>
-              As a <span> software engineer </span>with roots in biology and
-              ecology, I bring a unique perspective to coding. Drawing parallels
-              between the biological and coding realms, I've honed a perspective
-              that enables me to perceive how a line of code can influence the
-              broader application.
-            </p>
+            <h2>I'm an accessibility advocate.</h2>
+            <h3>I'm a problem solver.</h3>
+            <h4>I'm a creative.</h4>
 
             <p>
-              My background has allowed me to quickly learn and adapt to new
-              technologies, communicate effectively within a team, and apply an
-              <span> agile approach </span>to the development process. Combining
-              my cumulative skill set, I am eager to apply my abilities to forge
-              innovative solutions in climate, education, financial, or
-              customer-oriented technology. If you would like to chat more,
-              please feel free to contact me!
+              As a software engineer with a foundation in biology and ecology,
+              my coding perspective is shaped by a unique blend of technical
+              expertise and a passion for the natural world. Drawing inspiration
+              from outdoor adventures, unraveling complex puzzles, and embracing
+              new challenges, I bring creativity and adaptability to every
+              project.
+            </p>
+            <p>
+              Actively seeking opportunities in climate, education,
+              customer-focused, and financial tech, I welcome conversations and
+              connections. Feel free to reach out!
             </p>
           </div>
         </div>
-
-        {/* </div> */}
-        {/* <div className={styles.logoContainer}>
-              <Image src='/react-blue.png' alt='' height={50} width={50} />
-              <Image src='/nextjs.png' alt='' height={50} width={60} />
-              <Image src='/TypeScript.png' alt='' height={50} width={50} />
-              <Image src='/SASS_logo.png' alt='' height={50} width={50} />
-              <Image src='/Redux.png' alt='' height={50} width={60} />
-            </div> */}
       </div>
     </section>
   );
