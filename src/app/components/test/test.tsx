03cbf0e04fc5cf76327dc3b7c5cd4aaa6d2b726f
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-04 15:24:56 -0700

    Attempt to see if image will appear

--- src/app/components/test/test.tsx
+++ src/app/components/test/test.tsx
@@ -5,7 +5,13 @@ import Image from 'next/image';
 export default function TestComp() {
   return (
     <div className={styles.imgContainer}>
-      <Image src={'/cacti.jpg'} alt='' fill className={styles.img} />
+      <Image
+        src={'/cacti.jpg'}
+        alt=''
+        fill
+        className={styles.img}
+        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+      />
     </div>
   );
 }
