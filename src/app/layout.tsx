9ea847fe82781acdac82958a7ba8943565101149
Jocelyn Wensloff <jewensloff@gmail.com>
2024-02-19 09:14:33 -0700

    Add favicon as metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,12 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
